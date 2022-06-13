a4b735eea207e65a01926f9c4cbc147ba9271a77
[SW2/서울/1반] 김성은 <kse8538@gmail.com>
2022-06-14 00:22:24 +0900

    REFACTOR: account/update 스크립트 내에서 계정 확인

--- src/views/account/update.js
+++ src/views/account/update.js
@@ -1,44 +1,57 @@
-import { fetchUserData } from "./form-handler.js";
+import checkAccount from '../check-account.js';
+import drawNavbar from '../navbar/index.js';
+import { fetchUserData } from './form-handler.js';
 
-/*
+const ref = {
+    cartCount: document.getElementById('cart-count'),
+    updateBtns: document.querySelectorAll('.info_box button'),
+    changeArea: document.querySelectorAll('.change_area'),
+};
 
-    페이지가 로드되었을 때 서버에서 데이터를 받아서 화면에 보여줌
-    
-*/
+const drawCartCount = () => {
+    const cart = JSON.parse(localStorage.getItem('cart'));
 
-fetchUserData();
+    if (!cart) {
+        ref.cartCount.innerText = 0;
+    } else {
+        ref.cartCount.innerText = Object.keys(cart).length;
+    }
+};
 
-const showArea = document.querySelectorAll('.show_area button');
-const changeArea = document.querySelectorAll('.change_area');
+const setEvents = () => {
+    // 정보 변경 클릭 시 인풋 박스가 뜨는 화면 구성
+    ref.updateBtns.forEach((element) => {
+        element.addEventListener('click', (e) => {
+            const trElement = e.target.parentNode.parentNode;
+            trElement.classList.add('hide');
+            const changeTrElment = trElement.nextElementSibling;
+            changeTrElment.classList.remove('hide');
+        });
+    });
 
+    // 정보 변경 완료 버튼 클릭 시 데이터 fetch, 취소 클릭시 원래 화면
+    ref.changeArea.forEach((element) => {
+        const select = element.getAttribute('id');
+        const cancelBtn = element.querySelector(`#cancel_${select}_btn`);
 
-// 정보 변경 클릭 시 인풋 박스가 뜨는 화면 구성
-showArea.forEach((element) => {
-    element.addEventListener('click', (e) => {
-        const trElement = e.target.parentNode.parentNode;
-        trElement.classList.add('hide');
-        const changeTrElment = trElement.nextElementSibling;
-        changeTrElment.classList.remove('hide');
-    })
-})
+        cancelBtn.addEventListener('click', () => {
+            element.classList.add('hide');
+            element.previousElementSibling.classList.remove('hide');
+        });
+    });
+};
 
+const App = async () => {
+    const { isLogined, isAdmin } = await checkAccount();
 
-// 정보 변경 완료 버튼 클릭 시 데이터 fetch, 취소 클릭시 원래 화면
-changeArea.forEach((element) => {
-    const select = element.getAttribute('id');
-    const cancelBtn = element.querySelector(`#cancel_${select}_btn`);
+    if (!isLogined) {
+        alert('로그인이 필요한 페이지 입니다...');
+        window.location.href = '/';
+    }
 
-    cancelBtn.addEventListener('click', () => {
-        element.classList.add('hide');
-        element.previousElementSibling.classList.remove('hide');
-    })
-})
+    drawNavbar('update', isLogined, isAdmin);
+    drawCartCount();
+    fetchUserData();
+};
 
-
-
-
-
-
-
-
-
+App();
